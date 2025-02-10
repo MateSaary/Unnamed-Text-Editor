f42bb7f05ca50f75e44199c8f814e4f6c6a636ea
EmonSur <121367643@umail.ucc.ie>
2025-02-10 18:03:08 +0000

    Switch from Gio to Fyne

--- cmd/leda/leda.go
+++ cmd/leda/leda.go
@@ -1,25 +1,26 @@
 package main
 
 import (
-	"log"
-	"os"
-
-	"gioui.org/app"
-	"gioui.org/font/gofont"
-
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/app"
 	ui "github.com/Leda-Editor/Leda-Text-Editor/pkg/ui"
 )
 
 func main() {
-	th := ui.NewTheme(gofont.Collection())
-	ui := ui.NewUI(th)
+	// Initialize Fyne Application.
+	a := app.New()
 
-	go func() {
-		if err := ui.Loop(); err != nil {
-			log.Fatal(err)
-		}
-		os.Exit(0)
-	}()
+	// Create a new window for the application
+	w := a.NewWindow("Leda Text Editor")
 
-	app.Main()
+	// Initialize UI.
+	ledaUI := ui.NewUI(a, w)
+
+	// Set up window layout.
+	w.SetContent(ledaUI.Layout())
+
+	// Set window size.
+	w.Resize(fyne.NewSize(800, 600))
+	// Display the window and start the event loop.
+	w.ShowAndRun()
 }
--- pkg/ui/ui.go
+++ pkg/ui/ui.go
@@ -1,136 +1,58 @@
 package ui
 
 import (
-	"image"
-	"image/color"
-	"log"
-
-	"gioui.org/app"
-	"gioui.org/layout"
-	"gioui.org/op"
-	"gioui.org/op/clip"
-	"gioui.org/op/paint"
-	"gioui.org/unit"
-	"gioui.org/widget"
-	"gioui.org/widget/material"
-	"gioui.org/x/component"
-	"gioui.org/x/richtext"
-	"github.com/inkeliz/giohyperlink"
-
-	handling "github.com/Leda-Editor/Leda-Text-Editor/pkg/handling"
-)
-
-type (
-	C = layout.Context
-	D = layout.Dimensions
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/widget"
 )
 
 // UI specifies the user interface.
 type UI struct {
 	// External systems.
+	// Reference to the Fyne Application.
+	App fyne.App
 	// Window provides access to the OS window.
-	Window    *app.Window
-	// Theme contains semantic style data. Extends `material.Theme`.
-	Theme     *Theme
-	// Renderer tranforms raw text containing markdown into richtext.
-	Renderer  *handling.Renderer
+	Window fyne.Window
 	// Core state.
 	// Editor retains raw text in an edit buffer.
-	Editor    widget.Editor
+	Editor *widget.Entry
 	// TextState retains rich text interactions: clicks, hovers and longpresses.
-	TextState richtext.InteractiveText
-	// Resize state retains the split between the editor and the rendered text.
-	Resize    component.Resize
+	Markdown *widget.RichText
 }
 
 // NewUI initializes the UI.
-func NewUI(th *Theme) *UI {
-	return &UI{
-		Window:   new(app.Window),
-		Renderer: handling.NewRenderer(),
-		Theme:    th,
-		Resize:   component.Resize{Ratio: 0.5},
+func NewUI(app fyne.App, win fyne.Window) *UI {
+	ui := &UI{
+		App:    app,
+		Window: win,
+		// Creates a multi-line text editor.
+		Editor: widget.NewMultiLineEntry(),
+		// Initializes an empty Markdown preview.
+		Markdown: widget.NewRichTextFromMarkdown(""),
 	}
+
+	// Update Markdown Preview whenever text changes.
+	ui.Editor.OnChanged = func(content string) {
+		ui.RenderMarkdown(content)
+	}
+
+	return ui
 }
 
-// Loop drives the UI until the window is destroyed.
-func (ui *UI) Loop() error {
-	var ops op.Ops
-	for {
-		e := ui.Window.Event()
-		giohyperlink.ListenEvents(e)
-		switch e := e.(type) {
-		case app.DestroyEvent:
-			return e.Err
-		case app.FrameEvent:
-			gtx := app.NewContext(&ops, e)
-			ui.Layout(gtx)
-			e.Frame(gtx.Ops)
-		}
-	}
+// Updates Markdown Preview with proper markdown formatting.
+func (ui *UI) RenderMarkdown(input string) {
+	ui.Markdown.ParseMarkdown(input)
+	ui.Markdown.Refresh()
 }
 
-// Update processes events from the previous frame, updating state accordingly.
-func (ui *UI) Update(gtx C) {
-	for {
-		o, event, ok := ui.TextState.Update(gtx)
-		if !ok {
-			break
-		}
-		switch event.Type {
-		case richtext.Click:
-			if url, ok := o.Get("url").(string); ok && url != "" {
-				if err := giohyperlink.Open(url); err != nil {
-					log.Printf("error: opening hyperlink: %v", err)
-				}
-			}
-		case richtext.LongPress:
-			log.Println("longpress")
-			if url, ok := o.Get("url").(string); ok && url != "" {
-				ui.Window.Option(app.Title(url))
-			}
-		}
-	}
-
-	// Handle text changes
-	for {
-		event, ok := ui.Editor.Update(gtx)
-		if !ok {
-			break
-		}
-		if _, ok := event.(widget.ChangeEvent); ok {
-			var err error
-			ui.Theme.Cache, err = ui.Renderer.Render([]byte(ui.Editor.Text()))
-			if err != nil {
-				log.Printf("error: rendering markdown: %v", err)
-			}
-		}
-	}
-}
-
-// Layout renders the current frame.
-func (ui *UI) Layout(gtx C) D {
-	ui.Update(gtx)
-	return ui.Resize.Layout(gtx,
-		func(gtx C) D {
-			return layout.UniformInset(unit.Dp(4)).Layout(gtx, func(gtx C) D {
-				return material.Editor(ui.Theme.Base, &ui.Editor, "markdown").Layout(gtx)
-			})
-		},
-		func(gtx C) D {
-			return layout.UniformInset(unit.Dp(4)).Layout(gtx, func(gtx C) D {
-				return richtext.Text(&ui.TextState, ui.Theme.Base.Shaper, ui.Theme.Cache...).Layout(gtx)
-			})
-		},
-		func(gtx C) D {
-			rect := image.Rectangle{
-				Max: image.Point{
-					X: (gtx.Dp(unit.Dp(4))),
-					Y: (gtx.Constraints.Max.Y),
-				},
-			}
-			paint.FillShape(gtx.Ops, color.NRGBA{A: 200}, clip.Rect(rect).Op())
-			return D{Size: rect.Max}
-		},
+// Creates a split view with the editor and markdown preview.
+func (ui *UI) Layout() fyne.CanvasObject {
+	split := container.NewHSplit(
+		container.NewScroll(ui.Editor),
+		container.NewScroll(ui.Markdown),
 	)
+
+	split.SetOffset(0.5)
+
+	return split
 }
